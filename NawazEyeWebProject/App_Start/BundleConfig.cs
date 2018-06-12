f10f7894559fad4d3896f2b2d3223241c9f4e52f
Umair Tahir <professor.ut.786@gmail.com>
2018-06-12 13:50:02 +0500

    Initial Comit

---
+++ NawazEyeWebProject/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace NawazEyeWebProject
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.3.1.js",
+                        "~/Scripts/jquery-3.3.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customScripts").Include(
+                        "~/Scripts/npm.js",
+                        "~/Scripts/hotItemsSlider.js",
+                        "~/Scripts/LayoutCustomJquery.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrp.min.js",
+                      "~/Scripts/respond.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/style.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/customCss.css",
+                      "~/Content/w3-colors-flat.css",
+                      "~/Content/w3colors-metro.css"));
+        }
+    }
+}
